8d9c3573d36331e55bfdc942640e0d19057eac0a
Sachi Goyal <sachigoyal27@gmail.com>
2025-05-12 22:29:27 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { AnimatedShinyText } from "@/components/magicui/animated-shiny-text";
 import { ModeToggle } from "@/components/mode-toggle";
 import { CodeXml } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="w-full h-full font-sans">
+    <div className="w-full min-h-screen flex flex-col font-sans gap-5">
       <div className="flex flex-col items-center justify-center">
         <div className="flex justify-between items-center w-full mt-5 px-4">
           <div className="flex-1"></div>
@@ -16,11 +18,22 @@ export default function Home() {
             <ModeToggle />
           </div>
         </div>
-      <div className="text-muted-foreground mt-2 ml-6 text-lg italic ">
-        A simple wiki for all your code explanations
+        <div className="text-muted-foreground mt-2 ml-6 text-lg italic ">
+          A simple wiki for all your code explanations
+        </div>
       </div>
+      <div className="flex-1 container mx-auto p-4 max-w-7xl rounded-md flex flex-col">
+          <Tabs defaultValue="code" className="w-full mb-4">
+        <TabsList className="grid w-full grid-cols-2 ">
+          <TabsTrigger value="code" className="text-md cursor-pointer">Code</TabsTrigger>
+          <TabsTrigger value="explanation" className="text-md cursor-pointer">Explanation</TabsTrigger>
+        </TabsList>
+          </Tabs>
+        <div className="grid grid-cols-2 gap-4 h-full flex-1">
+          <Card className="p-4 flex items-center justify-center text-muted-foreground hover:shadow-md cursor-pointer">Paste your code snippets here</Card>
+          <Card className="p-4 flex items-center justify-center text-muted-foreground hover:shadow-md cursor-pointer">Explanation</Card>
+        </div>
       </div>
-      <div className="w-full"> hi</div>
     </div>
   );
 }
